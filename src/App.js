5b0ac4c939e3521aca6e8b6565bcfffb890b401f
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-06-04 19:18:46 +0530

    Accordian is working for now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
 import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 function App() {
   return (
     <Fragment>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </Fragment>
   );
